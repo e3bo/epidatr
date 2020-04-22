2430800fa77357f8f3edc963e5533503d1ec27e1
Eamon O'Dea <odea35@gmail.com>
2020-04-22 23:33:02 +0000

    Add useragent, error messages

--- R/api.R
+++ R/api.R
@@ -1,20 +1,36 @@
-epidata_api <- function(query){
+ua <- httr::user_agent("http://github.com/e3bo/epidatr")
+
+epidata_api <- function(query) {
   base_url <- "https://delphi.cmu.edu/epidata/api.php"
   url <- httr::modify_url(base_url, query = query)
-  resp <- httr::GET(url)
+  resp <- httr::GET(url, ua)
   if (httr::http_type(resp) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
-  parsed <- httr::content(resp, as = "parsed")
-  structure(
-    list(
-      content = parsed,
-      query = query,
-      response = resp
-    ),
-    class = "epidata_api"
-  )
+  parsed <- httr::content(resp, as = "parsed", simplifyVector = TRUE)
+
+  if (httr::http_error(resp) || parsed$result != 1) {
+    stop(
+      sprintf(
+        "epidata API request failed [%s]\nresult : %d\n%s\n<%s>",
+        httr::status_code(resp),
+        parsed$result,
+        parsed$message,
+        "https://github.com/cmu-delphi/delphi-epidata/tree/master/docs/api"
+      ),
+      call. = FALSE
+    )
+  }
+
+  structure(list(
+    content = parsed$epidata,
+    query = query,
+    response = resp
+  ),
+  class = "epidata_api")
 }
+
+
 
 print.epidata_api <- function(x, ...) {
   cat("<epidata ", x$query, ">\n", sep = "")
